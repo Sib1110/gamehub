9aa1377184cb169a0516e1cc83d99fc6691b7d5a
btn1110 <powhonor@gmail.com>
2023-04-25 07:42:55 +0900

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
   const { data: genres } = useGenres();
   return (
-    <ul>
-      {genres.map((d) => (
-        <li key={d.id}>{d.name}</li>
+    <List>
+      {genres.map((genre) => (
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 
